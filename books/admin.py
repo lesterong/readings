e23564fae6f2f4aea6b9fc5d01dfd7d98ab50291
Lester Ong <lesterong776@gmail.com>
2023-06-24 18:47:09 +0800

    Update admin panel for books

--- books/admin.py
+++ books/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "read_date"]
+    ordering = ["-read_date"]
+
+
+admin.site.register(Book, BookAdmin)
